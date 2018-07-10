2920514b0613068001f010cef96f0651536231ae
mcmido <mcmido93@gmail.com>
2018-07-10 21:25:41 +0300

    Terminal Made
    Php files added / interface made

---
+++ app/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>awebarts-PHP</title>
+
+	<link rel="stylesheet" type="text/css" href="resources/css/bootstrap.bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="resources/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="resources/css/style.css">
+
+	<script src="resources/js/bootstrap.min.js" type="text/javascript" charset="utf-8" async defer></script>
+	<script src="resources/js/bootstrap.min.js" type="text/javascript" charset="utf-8" async defer></script>
+
+</head>
+<body>
+	<div class="container">
+	<!-- Header -->
+	<header>
+		<img src="resources/images/logo.png" alt="logo">
+		<h2>Welcome Admin</h2>
+	</header>
+	<!-- /header -->
+	<div class="clear"></div>
+	<!-- Clear fix -->
+	<div id="contents">
+		<aside>
+			<nav>
+				<a class="btn btn-danger active" href="index.php">Home Pages</a></li>
+				<a class="btn btn-danger " href="?page=MainSettings">Main Settings</a></li>
+				<a class="btn btn-danger" href="?page=Sections">Sections</a></li>
+				<a class="btn btn-danger" href="?page=Pages">Pages</a></li>
+				<a class="btn btn-danger" href="?page=Comments">Comments</a></li>
+				<a class="btn btn-danger" href="?page=Library">Library</a></li>
+			</nav>
+		</aside>
+		<section id="page">
+			<?php
+				if(@$_GET['page'])
+				{
+					$url = $_GET['page'].".php";
+					if(is_file($url))
+					{
+						include $url;
+					}
+					else
+					{
+						echo "Requested file is not found";
+					} 
+				}
+				else
+				{
+					include './main.php';
+				}
+
+			?>
+		</section>
+	</div>
+	<!-- Clear fix -->
+	<div class="clear"></div>
+	<!-- Footer -->
+	<footer>
+		<p>Copyrights Reserved - Mohamed Farag</p>
+	</footer>
+	<!-- /Footer -->
+	</div>
+</body>
+</html>
